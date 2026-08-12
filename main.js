5cbe71718f46b71036a66a52d43144771514ee96
ANAPAULA05082008 <a.batista05@escola.pr.gov.br>
2026-08-12 16:25:36 -0300

    12/08/26

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ NumeroSenha.textContent=5;
 const botoes=document.queryselector('parametro-senha_botao');
 botoes(0).onclick=diminuiTamanho;
 funcion diminuiTamanho () {
-tamanhoSenha=tamanhoSenha-1;
+if(tamanhoSenha-1;
 numeroSenha.textContent=tamanhoSenha;
 }
 
